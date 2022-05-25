3317fd15dd10e4379a2282507ef59ceee7e72a7f
gmshamca <100260790+gmshamca@users.noreply.github.com>
2022-05-25 18:57:03 +0530

    Update variable.tf

--- modules/build_trigger_build/variable.tf
+++ modules/build_trigger_build/variable.tf
@@ -1,1 +1,23 @@
 
+variable "repo_type" {
+  type        = string
+  description = "The name of the repo Type"
+  default = "GITHUB"
+}
+
+variable "git_repo" {
+  description = "Git Repository Details"
+  type = map
+  default = {
+      repo1 = {
+        name           = "app1"
+        uri            = "https://github.com/gmshamca/MicroIngramTerraformmNew.git"
+        branch         = "main"
+    },
+ 
+    repo2 = {
+        name              = "app2"
+        uri               = "https://github.com/gmshamca/gmsha-terraform-cloud.git"
+        branch            = "main"
+   }
+ }
