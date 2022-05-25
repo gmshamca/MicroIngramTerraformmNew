eae9237e539b62711d7791accc591d23d3fa275c
gmshamca <100260790+gmshamca@users.noreply.github.com>
2022-05-25 17:14:32 +0530

    Update variable.tf

--- modules/variable.tf
+++ modules/variable.tf
@@ -1,1 +1,24 @@
 
+variable "repo_type" {
+  type        = string
+  description = "The name of the repo Type"
+  default = "GITHUB"
+}
+
+variable "git_repo" {
+  description = "Git Repository Details"
+  type = map
+  default = {
+      repo1 = {
+        name           = "app1"
+        uri            = "https://github.com/anup1987/spring-boot-hello-world-1"
+        branch         = "master"
+    },
+ 
+    repo2 = {
+        name              = "app2"
+        uri               = "https://github.com/anup1987/cloud-build-samples/tree/main/maven-example"
+        branch            = "main"
+   }
+ }
+}
