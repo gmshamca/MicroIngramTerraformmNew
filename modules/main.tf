2e96be8fcb922566b94779d6103d802c4988f55c
gmshamca <100260790+gmshamca@users.noreply.github.com>
2022-05-25 17:13:26 +0530

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -1,1 +1,19 @@
 
+resource "google_cloudbuild_trigger" "manual-trigger" {
+  for_each  = var.git_repo
+  name        = each.value.name
+  
+  source_to_build {
+    uri       = each.value.uri
+    ref       = each.value.branch
+    repo_type = var.repo_type
+  }
+
+  git_file_source {
+    path      = "cloudbuild.yaml"
+    uri       =  each.value.uri
+    revision  =  each.value.branch
+    repo_type = var.repo_type
+  }
+
+}
