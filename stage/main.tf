5dca4c13d199f812ed3bcec03c19609ed991c4a2
gmshamca <100260790+gmshamca@users.noreply.github.com>
2022-06-01 13:43:28 +0530

    Update main.tf

--- stage/main.tf
+++ stage/main.tf
@@ -1,6 +1,6 @@
 
 resource  "google_cloudbuild_trigger" "manual-trigger" {
-  repo_type = var.repo_type
-  git_repo= var.git_repo
+  repotype = var.repo_type
+  gitrepo= var.git_repo
 #   source               = "../modules/build_trigger_build"
 }
