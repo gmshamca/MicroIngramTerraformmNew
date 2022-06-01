f7381b2799fce27d61584b5756b7ff7a0bf9b222
gmshamca <100260790+gmshamca@users.noreply.github.com>
2022-06-01 17:10:23 +0530

    Update variable.tf

--- stage/variable.tf
+++ stage/variable.tf
@@ -1,12 +1,12 @@
 
 variable "repo_type" {
   type        = string
-#   description = "The name of the repo Type"
+
 }
 
 variable "repo" {
   type = string
-#   description = "Git Repository Details"
+
  
 }
 
