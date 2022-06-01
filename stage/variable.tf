8674baa9df08403607319110f924107fca27048d
gmshamca <100260790+gmshamca@users.noreply.github.com>
2022-06-01 16:43:06 +0530

    Update variable.tf

--- stage/variable.tf
+++ stage/variable.tf
@@ -4,8 +4,15 @@ variable "repo_type" {
   description = "The name of the repo Type"
 }
 
-# variable "repo_details" {
-#   type = string
-#   description = "Git Repository Details"
-
-# }
+variable "repo_details" {
+  type = string
+  description = "Git Repository Details"
+ type = map
+  default = {
+      repo1 = {
+        name           = "git1"
+        uri            = "https://github.com/gmshamca/spring-build--trigger"
+        branch         = "main"
+    }
+}
+}
