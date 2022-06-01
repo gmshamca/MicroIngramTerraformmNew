f1bb4532aac19e331aab3717c20fc3638aae3e02
gmshamca <100260790+gmshamca@users.noreply.github.com>
2022-06-01 18:13:23 +0530

    Create variables.tf

---
+++ cloud_build_trigger/variables.tf
@@ -0,0 +1,21 @@
+variable "repo_type" {
+  type        = string
+  description = "The name of the repo Type"
+  default = "GITHUB"
+}
+
+
+variable "name" {
+  type        = string
+  description = "The name of the Trigger"
+}
+
+variable "uri" {
+  type        = string
+  description = "The name of the URI"
+}
+
+variable "branch" {
+  type        = string
+  description = "The name of the Branch"
+}
