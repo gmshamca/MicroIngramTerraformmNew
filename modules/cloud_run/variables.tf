25c774db405f44b0010fcebf43df3aa3f349b5ea
gmshamca <100260790+gmshamca@users.noreply.github.com>
2022-06-01 18:17:22 +0530

    Create variables.tf

---
+++ modules/cloud_run/variables.tf
@@ -0,0 +1,20 @@
+variable "deploy_run_service_name" {
+  type        = string
+  description = "Cloud Run Service Name"
+}
+
+variable "location" {
+  type        = string
+  description = "Region for Deployment"
+  default = "us-central1"
+}
+
+variable "imagename" {
+  type        = string
+  description = "Name of the image"
+}
+
+variable "project_id" {
+  type        = string
+  description = "Project ID"
+}
