d4443083bfeedba7ba44793a55bebece5591058e
gmshamca <100260790+gmshamca@users.noreply.github.com>
2022-06-03 11:22:09 +0530

    Update main.tf

--- stage/build/main.tf
+++ stage/build/main.tf
@@ -3,6 +3,6 @@ module "build-trigger_and_run" {
   source               = "../modules/cloud_build_trigger"
   name      = each.value.name
   uri       = each.value.uri
-  branch    = "master"
+  branch    = "main"
   repo_type = var.repo_type   
 }
