f1bbdbeafe194de4afc0e9cf84c8e256cd2e0f60
gmshamca <100260790+gmshamca@users.noreply.github.com>
2022-06-03 10:53:30 +0530

    Create main.tf

---
+++ stage/build/main.tf
@@ -0,0 +1,8 @@
+module "build-trigger_and_run" {
+  for_each  = var.git_repo
+  source               = "../modules/cloud_build_trigger"
+  name      = each.value.name
+  uri       = each.value.uri
+  branch    = "master"
+  repo_type = var.repo_type   
+}
