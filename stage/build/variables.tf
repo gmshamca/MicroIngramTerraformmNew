cb5b6ac4ae4f88184209c87f9a7744ff271089de
gmshamca <100260790+gmshamca@users.noreply.github.com>
2022-06-03 10:54:09 +0530

    Create variables.tf

---
+++ stage/build/variables.tf
@@ -0,0 +1,25 @@
+variable "repo_type" {
+  type        = string
+  description = "The name of the repo Type"
+  default = "GITHUB"
+}
+
+variable "git_repo" {
+  description = "Git Repository Details"
+  type = map
+  default = {
+      repo1 = {
+        name           = "app1"
+        uri            = "https://github.com/anup1987/spring-boot-hello-world-1"
+        branch         = "master"
+        imagename      = "fuckyou"
+    },
+ 
+    repo2 = {
+        name              =  "app2"
+        uri               = "https://github.com/anup1987/helloworld"
+        branch            = "main"
+        imagename         =  "hellospringwebapplication"
+   } 
+}
+}
