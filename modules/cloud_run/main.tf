472a0247660ac58475be7d03ea1dd544a49bd835
gmshamca <100260790+gmshamca@users.noreply.github.com>
2022-06-04 12:24:26 +0530

    Update main.tf

--- modules/cloud_run/main.tf
+++ modules/cloud_run/main.tf
@@ -17,7 +17,7 @@ resource "google_cloud_run_service" "deployrun" {
     google_project_service.run
   ]
   
-   name     = var.serviceName
+   name     = var.servicename
    location = var.location
    autogenerate_revision_name = true
   
