f5c4682ef07476d93fabe8b2bceb3d195bc998c9
gmshamca <100260790+gmshamca@users.noreply.github.com>
2022-06-07 13:17:41 +0530

    Update main.tf

--- stage/build/main.tf
+++ stage/build/main.tf
@@ -1,4 +1,4 @@
-module "build-trigger_and_run" {
+module "cloud_build_trigger_run" {
   for_each  = var.git_repo
   source               = "../modules/cloud_build_trigger"
   name      = each.value.name
