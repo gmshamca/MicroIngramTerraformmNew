7900f5545ff7a58d7cffe6cfd5172437469be50a
gmshamca <100260790+gmshamca@users.noreply.github.com>
2022-06-09 12:27:31 +0530

    Update main.tf

--- modules/cloud_run/main.tf
+++ modules/cloud_run/main.tf
@@ -2,13 +2,44 @@ provider "google" {
    project = var.project_id
 }
 
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.73.0"
+    }
+  }
+
+  required_version = ">= 0.15.0"
+}
+
+provider "google" {
+  project = var.project_id
+  region  = "us-central1"
+  }
+
+# module "project_services" {
+#   source  = "terraform-google-modules/project-factory/google//modules/project_services"
+#   version = "3.3.0"
+
+#   project_id = "{{project-id}}"
+
+#   activate_apis = [
+#     "compute.googleapis.com",
+#     "oslogin.googleapis.com"
+#   ]
+
+#   disable_services_on_destroy = false
+#   disable_dependent_services  = false
+# }
+
 data "google_container_registry_image" "gcr" {
   name = var.imagename
 }
 
 resource "google_project_service" "run" {
-  service            = "run.googleapis.com"
-#   disable_on_destroy = false
+  service            = "compute.googleapis.com"
+   disable_on_destroy = false
 }
 
 resource "google_cloud_run_service" "deployrun" {
